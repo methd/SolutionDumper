af55006a1307924020885d1191c9543aea803534
methd <46592259+methd@users.noreply.github.com>
2026-01-13 14:56:01 +0400

    replace System.Drawing brushes with WPF Media brushes

--- SolutionDumper/Converters/BoolToGrayBrushConverter.cs
+++ SolutionDumper/Converters/BoolToGrayBrushConverter.cs
@@ -1,6 +1,6 @@
-﻿using System.Drawing;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace SolutionDumper.Converters;
 
@@ -10,12 +10,7 @@ public sealed class BoolToGrayBrushConverter : IValueConverter
     public Brush DisabledBrush { get; set; } = Brushes.Gray;
 
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        if (value is bool b && b)
-            return NormalBrush;
-
-        return DisabledBrush;
-    }
+        => value is bool b && b ? NormalBrush : DisabledBrush;
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => Binding.DoNothing;
